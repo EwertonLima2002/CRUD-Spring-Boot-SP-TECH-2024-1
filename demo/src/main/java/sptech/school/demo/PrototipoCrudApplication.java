a488a6afd2c785a7668b6c94b5ce7bcbf4fbd774
EwertonLima2002 <ewerton.lima860@sptech.school>
2024-02-11 17:33:38 -0300

    initial commit

---
+++ demo/src/main/java/sptech/school/demo/PrototipoCrudApplication.java
@@ -0,0 +1,13 @@
+package sptech.school.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototipoCrudApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/sptech/school/demo/PrototipoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.school.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
